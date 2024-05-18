96628535a5578e1e8d66eb6e610c65a899163603
Wonjung <gimpo5975@naver.com>
2024-05-18 13:21:44 +0900

    재 빌드

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-// src/index.js
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client'; // Note the change here
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+// Create a root
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Render your app
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 reportWebVitals();
