d63075245105a6300df14a13d8df8bec9846764d
Wonjung <gimpo5975@naver.com>
2024-05-18 13:49:37 +0900

    깃 액션 테스트

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
       <h1 className="text-4xl font-bold text-blue-600">Hello, Tailwind CSS!</h1>
+      <h2>하잉</h2>
     </div>
   );
 }
