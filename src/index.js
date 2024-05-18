a905aaf35a080ca310e0679067ec87baddec847e
Wonjung <gimpo5975@naver.com>
2024-05-18 13:20:57 +0900

    테스트 2

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import './tailwind.css'; // Import Tailwind CSS
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
